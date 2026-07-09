365370a844f09622f0554ff34989d5201af133bf
Denys Kondratiuk <dkondratiuk@pm.me>
2026-07-09 17:55:35 +0200

    [home-manager] update tmux
    - set prefix to Ctrl-A
    - add plugins
      - nord theme for status line
      - resurrect for session management
      - continuum for auto-save/auto-load sessions
    - index panes and windows from 1

--- .config/home-manager/tmux.nix
+++ .config/home-manager/tmux.nix
@@ -7,12 +7,25 @@
     plugins = with pkgs; [
       tmuxPlugins.sensible
       tmuxPlugins.cpu
+      tmuxPlugins.nord
+      tmuxPlugins.resurrect
+      tmuxPlugins.continuum
     ];
     extraConfig = ''
+      set -g prefix C-a
+      unbind C-b
+      bind C-a send-prefix
+      set -g status-position top
       # set -ga terminal-overrides ",*-256color:Tc"
       set -g default-terminal "alacritty"
       set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'  # colored underscores
       set -as terminal-overrides ',alacritty:RGB' # true-color support
+      # Start windows and panes at 1, not 0
+      set -g base-index 1
+      setw -g pane-base-index 1
+      # Plugins
+      set -g @continuum-restore 'on' # Last saved environment is automatically restored when tmux is started.
+      set -g @continuum-boot-options 'alacritty,fullscreen'
     '';
   };
 }
